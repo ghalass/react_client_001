2deb26c6da8b89e1b9de223dfae428e83b7c42f3
ghalass <msghalas@gmail.com>
2023-08-27 23:17:17 +0000

    datatable for siteslist page

--- src/components/sites/SitesList.jsx
+++ src/components/sites/SitesList.jsx
@@ -1,15 +1,104 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import DataTable from "react-data-table-component";
+import moment from "moment";
+import { Alert } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 function SitesList() {
+  const navigate = useNavigate();
+  const columns = [
+    {
+      name: "Title",
+      selector: (row) => row.title.toUpperCase(),
+      sortable: true,
+    },
+    {
+      name: "Description",
+      selector: (row) => row.description.toUpperCase(),
+      sortable: true,
+    },
+    {
+      name: "CreatedAt",
+      selector: (row) => moment(row.createdAt).format("ll"),
+      sortable: true,
+    },
+    {
+      name: "UpdatedAt",
+      selector: (row) => moment(row.updatedAt).format("ll"),
+      sortable: true,
+    },
+  ];
+
+  const records = [
+    {
+      id: "1",
+      title: "to14",
+      description: "desc",
+    },
+    {
+      id: "2",
+      title: "pg11",
+      description: "desc",
+    },
+    {
+      id: "2",
+      title: "pg10",
+      description: "desc",
+    },
+    {
+      id: "3",
+      title: "fdeirek",
+      description: "desc",
+    },
+  ];
+
   return (
     <div>
-      <Link to="1">Site 1 </Link>
-      <br />
-      <Link to="2">Site 2 </Link>
-      <br />
-      <Link to="3">Site 3 </Link>
-      <br />
+      <DataTable
+        // title="Liste des sites :"
+        noDataComponent={
+          <Alert key={"idx"} variant={"info"} className="my-2">
+            <FontAwesomeIcon icon={faInfoCircle} className="mr-2" />
+            Il n'y a aucun enregistrement à afficher
+          </Alert>
+        }
+        columns={columns}
+        data={records}
+        dense="true"
+        // direction="auto"
+        fixedHeader="true"
+        fixedHeaderScrollHeight="400px"
+        highlightOnHover="true"
+        pointerOnHover="true"
+        responsive="true"
+        // subHeader="true"
+        // subHeaderAlign="right"
+        // subHeaderWrap="true"
+        persistTableHead="true"
+        /* selectableRows="true" */
+        /* selectableRowsSingle="true"
+        selectableRowsHighlight="true"
+        selectableRowsNoSelectAll="true"
+        selectableRowsVisibleOnly="true" */
+        /* progressPending={isLoading} */
+        progressComponent="Loading ..."
+        onRowClicked={(row, event) => {
+          // console.log(event);
+          // setObject(row);
+          navigate(`/config/sites/${row.id}`);
+        }}
+        onSelectedRowsChange={({
+          allSelected,
+          selectedCount,
+          selectedRows,
+        }) => {
+          /* setObject(selectedRows.length !== 0 ? selectedRows[0] : {});
+          setOperation(""); */
+        }}
+        pagination="true"
+      />
     </div>
   );
 }
