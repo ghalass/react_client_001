618e3fb22afdfacdae5c07f1348598e143874cf9
ghalass <msghalas@gmail.com>
2023-08-28 19:09:44 +0000

    mainLoader

---
+++ src/components/loaders/MainLoader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./mainLoader.css";
+
+function MainLoader() {
+  return <div className="mainLoader">Loading ...</div>;
+}
+
+export default MainLoader;
--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -5,37 +5,47 @@ import { Container } from "react-bootstrap";
 import MainSideBar from "../components/sidebar/MainSideBar";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import MainLoader from "../components/loaders/MainLoader";
 
 function MainLayout() {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 3000);
+    }, 5000);
   }, []);
 
   return (
     <div>
-      <Header />
-      <Container fluid className="p-2 mainContainer">
-        <MainSideBar />
-        <div className="text-left">
-          <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={true}
-            closeOnClick={true}
-            rtl={false}
-            pauseOnHover={false}
-            pauseOnFocusLoss={false}
-            draggable={false}
-            progress={undefined}
-            theme="light"
-          />
-        </div>
-        <div className="main">{loading ? "Loading ..." : <Outlet />}</div>
-      </Container>
+      <div className="main">
+        {loading ? (
+          <MainLoader />
+        ) : (
+          <>
+            <Header />
+            <Container fluid className="p-2 mainContainer">
+              <MainSideBar />
+              <div className="text-left">
+                <ToastContainer
+                  position="top-right"
+                  autoClose={5000}
+                  hideProgressBar={false}
+                  newestOnTop={true}
+                  closeOnClick={true}
+                  rtl={false}
+                  pauseOnHover={false}
+                  pauseOnFocusLoss={false}
+                  draggable={false}
+                  progress={undefined}
+                  theme="light"
+                />
+              </div>
+
+              <Outlet />
+            </Container>
+          </>
+        )}
+      </div>
     </div>
   );
 }
