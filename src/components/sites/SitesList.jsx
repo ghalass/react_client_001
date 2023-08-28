b5410ec79d86fb92b6bd03ed8a69e34d66130a31
ghalass <msghalas@gmail.com>
2023-08-28 10:30:16 +0000

    sites list page

--- src/components/sites/SitesList.jsx
+++ src/components/sites/SitesList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import DataTable from "react-data-table-component";
 import moment from "moment";
 import { Alert } from "react-bootstrap";
